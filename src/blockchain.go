03c54b375aad33b8b5e86e2e3baa4ae5b13b4bf2
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2024-10-18 01:16:20 -0700

    Update blockchain.go
    formatting issue

--- src/blockchain.go
+++ src/blockchain.go
@@ -22,21 +22,21 @@ import (
 type block struct {
 	Index        	int
 	Initials     	string
-	Sex			 	string
-	Gender		 	string
+	Sex		string
+	Gender		string
 	Age          	int
 	Height       	float32
 	Weight       	float32
-	BMI				float32
-	Blood		 	string
+	BMI		float32
+	Blood		string
 	Time         	string
-	Location		string
+	Location	string
 	PreviousHash 	string
 	CurrentHash  	string
 	Prescriptions	[]string
 	Conditions   	[]string
-	VisitLogs		[]string
-	History			[]string
+	VisitLogs	[]string
+	History		[]string
 }
 
 type chain struct {
